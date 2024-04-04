137ea138e9b4fa919d4f48a7d19522570c36e308
Aiman Fadillah <mlakece914@gmail.com>
2024-04-04 11:00:59 +0700

    menambahkan beberapa route baru

--- backend/index.js
+++ backend/index.js
@@ -4,37 +4,74 @@ const cheerio = require("cheerio");
 const cors = require("cors");
 const endpoint = "https://rebahinxxi.shop"
 
-app.use(cors());
-
-app.get("/films", async (req, res) => {
-    const page = req.query.page || 1;
-    const search = req.query.s || undefined;
-    let response = { status: 400 };
-    let loop = 1;
-    while (response.status != 200 && loop != 3) {
-        try {
-            response = await axios.get(`${endpoint}${search ? "" : "/movies"}/page/${page}/${search ? `?s=${search}` : ""}`);
-        } catch {
-            loop++
-        }
-    }
-    if (loop == 3) {
+app.get("/recommended",async (req,res) => {
+    try{
+        const page = req.query.page || 1;
+        const search = req.query.s || undefined;
+        const response = await axios.get(`${endpoint}/genre/rekomendasi-film/page/${page}/${search ? `?s=${search}` : ""}`);
+        const $ = cheerio.load(response.data);
+        const data = [];
+        $(".movies-list-full").find("div.ml-item").each((index, element) => {
+            data.push({
+                nama: $(element).find("h2").text(),
+                image: $(element).find("img").attr("src"),
+                rating: $(element).find("div.rating-durasi > span.mli-rating").text(),
+                durasi: $(element).find("div.rating-durasi > span.mli-durasi").text(),
+                kualitas: $(element).find("span.mli-quality").text(),
+                slug: (($(element).find("a").attr("href")).split("/"))[3]
+            });
+        });
+        return res.status(200).json(data);
+    }catch(e){
         return res.status(200).json([]);
     }
-    const $ = cheerio.load(response.data);
-    const data = [];
-    $(".movies-list-full").find("div.ml-item").each((index, element) => {
-        data.push({
-            nama: $(element).find("h2").text(),
-            image: $(element).find("img").attr("src"),
-            rating: $(element).find("div.rating-durasi > span.mli-rating").text(),
-            durasi: $(element).find("div.rating-durasi > span.mli-durasi").text(),
-            kualitas: $(element).find("span.mli-quality").text(),
-            slug: (($(element).find("a").attr("href")).split("/"))[3]
-        });
-    });
-    return res.status(200).json(data);
+})
 
+app.get("/cam",async (req,res) => {
+    try{
+        const page = req.query.page || 1;
+        const search = req.query.s || undefined;
+        const response = await axios.get(`${endpoint}/quality/cam/page/${page}/${search ? `?s=${search}` : ""}`);
+        const $ = cheerio.load(response.data);
+        const data = [];
+        $(".movies-list-full").find("div.ml-item").each((index, element) => {
+            data.push({
+                nama: $(element).find("h2").text(),
+                image: $(element).find("img").attr("src"),
+                rating: $(element).find("div.rating-durasi > span.mli-rating").text(),
+                durasi: $(element).find("div.rating-durasi > span.mli-durasi").text(),
+                kualitas: $(element).find("span.mli-quality").text(),
+                slug: (($(element).find("a").attr("href")).split("/"))[3]
+            });
+        });
+        return res.status(200).json(data);
+    }catch(e){
+        return res.status(200).json([]);
+    }
+})
+
+
+app.get("/films", async (req, res) => {
+    try{
+        const page = req.query.page || 1;
+        const search = req.query.s || undefined;
+        const response = await axios.get(`${endpoint}${search ? "" : "/movies"}/page/${page}/${search ? `?s=${search}` : ""}`);
+        const $ = cheerio.load(response.data);
+        const data = [];
+        $(".movies-list-full").find("div.ml-item").each((index, element) => {
+            data.push({
+                nama: $(element).find("h2").text(),
+                image: $(element).find("img").attr("src"),
+                rating: $(element).find("div.rating-durasi > span.mli-rating").text(),
+                durasi: $(element).find("div.rating-durasi > span.mli-durasi").text(),
+                kualitas: $(element).find("span.mli-quality").text(),
+                slug: (($(element).find("a").attr("href")).split("/"))[3]
+            });
+        });
+        return res.status(200).json(data);
+    }catch(e){
+        return res.status(200).json([]);
+    }
 })
 
 app.get("/films/:slug", async (req, res) => {
